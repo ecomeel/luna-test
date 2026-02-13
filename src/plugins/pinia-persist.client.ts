8f38394b966b381d41cc513aaff49940f6af77f4
aleks.pulin <anastasia@MacBook-Air-Anastasia.local>
2026-02-13 23:53:21 +0300

    feat(piniaPersistedState) - use pinia persisted package

--- src/plugins/pinia-persist.client.ts
+++ src/plugins/pinia-persist.client.ts
@@ -1,34 +1,6 @@
-import type { PiniaPluginContext } from "pinia";
-
-type PersistOptions = {
-  key: string;
-};
-
-declare module "pinia" {
-  export interface DefineStoreOptionsBase<S, Store> {
-    persist?: PersistOptions;
-  }
-}
-
-function persistPlugin({ store, options }: PiniaPluginContext) {
-  const key = options.persist?.key;
-  if (!key) return;
-
-  const raw = localStorage.getItem(key);
-  if (raw) {
-    try {
-      store.$patch(JSON.parse(raw));
-    } catch {
-      console.log('fail with storage')
-    }
-  }
-
-  store.$subscribe((_mutation, state) => {
-    localStorage.setItem(key, JSON.stringify(state));
-  });
-}
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 export default defineNuxtPlugin(() => {
   const pinia = usePinia();
-  pinia.use(persistPlugin);
+  pinia.use(piniaPluginPersistedstate);
 });
